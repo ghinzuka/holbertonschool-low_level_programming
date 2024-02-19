55e38ec8c678f9fdfc83307e964d784128da3e29
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-19 09:39:20 +0000

    write a C programm that print the CHAR string using the PUT fonction

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,9 @@
-#!/bin/bash
-#include<stdio.h>
-int main(1)
-{
-	
-    char Mystr[2] = ""Programming is like building a multilingual puzzle";
 
-    puts(Mystr); 
+#include<stdio.h>
+int main(void)
+{
+
+    puts("\"Programming is like building a multilingual puzzle"); 
 
     return (0);
 }
