2d7137b210c13e310279dae58fb29624b6034b91
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-19 10:21:08 +0000

    print using printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - main functon
+ *
+ * Description: 'the function printf to write a char string with '
+ *
+ * Return: Always return  0
+ */
+
+int main(void)
+{
+	printf(with proper grammar, but the outcome is a piece of art,\n);
+
+	return (0);
+}
