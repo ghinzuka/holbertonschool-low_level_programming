5f6efce4f4ddd96cfcc094c3b6b97b780c4db680
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-19 15:50:43 +0000

    print all sing digit of base 10 starting from 0

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+
+/**
+ * main - main functon
+ *
+ * Description: 'the function should print all single digit starting from 0'
+ *
+ * Return: Always return  0
+ */
+
+int main(void)
+
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		printf("%d, n");
+	}
+
+	printf('\n');
+
+	return (0);
+}
