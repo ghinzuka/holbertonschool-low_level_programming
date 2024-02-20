031fc5785d6415999dba03beab77966636c906ce
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-20 15:23:04 +0000

    commit isalpha to check for alphabetical character

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabetic character
+ *
+ * Description: 'check if c is an alphabetical character'
+ *  @c: character to check
+ *
+ * Return: 1 if lowercase
+ * Return 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		if (c >= 'A' && c <= 'Z')
+		{
+		return (1);
+		}
+	}
+
+	return (0);
+
+}
