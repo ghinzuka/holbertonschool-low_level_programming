78301bf3de2dc9b34c1b16ec975af62cfe7077f0
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-20 13:45:04 +0000

    print alphabet using prototype

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - main functon
+ *
+ * Description: 'print _putchar using write function.'
+ *
+ * Return: Always return 0
+ */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+
+		letter++;
+	}
+	_putchar('\n');
+	return (0);
+}
