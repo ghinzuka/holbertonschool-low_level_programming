5a2fb831741e78f21495104457491a926353ec9e
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-20 12:52:01 +0000

    commit the programm caling the main.h header

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,17 +1,25 @@
-#include <stdio.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
  * main - main functon
  *
- * Description: 'print _putchar using  write function.'
+ * Description: 'print _putchar using write function.'
  *
- * Return: Always return  0
+ * Return: Always return 0
  */
 
 int main(void)
 {
-	write(1, "_putchar", 9);
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
