aeae14daa3007e6e69260f61d8d6c5d9f294f98a
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-20 08:15:40 +0000

    print digits using putchar according to the ASCII reference

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+
+/**
+ * main - main functon
+ *
+ * Description: 'print the alphabet in lowercase except q & e'
+ *
+ * Return: Always return  0
+ */
+
+int main(void)
+
+{
+        char n;
+
+        for (n = '0'; n < '9'; n++)
+        {
+                putchar('0' + n);
+        }
+
+        putchar('\n');
+        return (0);
+}  
