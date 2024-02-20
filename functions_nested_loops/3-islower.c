32699084a3860f304da290c8da8094a4e46f098a
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-20 14:54:58 +0000

    check if lowercase or nor

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - check for lowercase
+ *
+ * Description: 'print the alphabet using while loop and _putchar proto'
+ *
+ * Return: 1 if lowercase
+ * Return 0 otherwise
+ */
+
+int _islower(int c)
+}
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	
+	}
+
+	return (0);
+
+}
