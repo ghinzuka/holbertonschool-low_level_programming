090bf99646c06096e01e4c92664baccb22e3d42d
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-20 15:43:21 +0000

    Write a function that prints the sign of a number.

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -17,19 +17,17 @@ int print_sign(int n)
 	{
 		return (1);
 		_putchar('+');
+	}
 		else if (n == 0)
 		{
 			return (0);
 			_putchar('0');
+		}
 			else if (n < 0)
 			{
 				return (-1);
 				_putchar('-');
 			}
-		}
-
-
-	}
-
+return (0);
 
 }
