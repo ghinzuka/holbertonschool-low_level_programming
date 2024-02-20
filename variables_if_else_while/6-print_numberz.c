483e4c9912e0151221a5c8b96366d3e04e46e497
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-20 08:23:19 +0000

    print digits using putchar according to the ASCII reference

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -14,13 +14,13 @@
 int main(void)
 
 {
-        int n;
+	int n;
 
-        for (n = 0; n < 10; n++)
-        {
-                putchar('0' + n);
-        }
+	for (n = 0; n < 10; n++)
+	{
+		putchar('0' + n);
+	}
 
-        putchar('\n');
-        return (0);
-}  
+	putchar('\n');
+	return (0);
+}
