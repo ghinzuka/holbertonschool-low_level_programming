21180067531cc2489989d4394c3a14ffc71b97a5
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-20 09:54:02 +0000

    print _putchar using printf function

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+
+/**
+ * main - main functon
+ *
+ * Description: 'print the alphabet in lowercase except q & e'
+ *
+ * Return: Always return  0
+ */
+
+int main(void)
+
+{
+	printf("_putcharn");
+
+	return (0);
+}
