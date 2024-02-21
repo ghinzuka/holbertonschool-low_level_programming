3b582a1f372cbc5c46d3b775fe6ac05ee9d70c25
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-21 10:54:03 +0000

    push a function that prints with _putchar using the % 100 for the last digit

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - show every minute from 00:00 to 23:59
+ * @first: hours
+ * @second: Minutes
+ * Return: 0 if code ok
+ */
+
+void jack_bauer(void)
+{
+	int first, second;
+
+	for (first = 0; first <= 23; first++)
+	{
+		for (second =0; second <=59; second++)
+		{
+		_putchar((first % 100) + '0');
+		_putchar(':');
+		_putchar((second % 100) +'0');
+		_putchar('\n');
+		}
+	}
+	return (0);
+
+}
