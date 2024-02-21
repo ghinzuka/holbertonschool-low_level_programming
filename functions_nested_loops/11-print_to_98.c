6ceb221f935df46c2071419a52ed40ec8b868611
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-21 14:48:23 +0000

    function add tw<o integers

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * jack_bauer - show every minute from 00:00 to 23:59
+ * Return: 0 if code ok
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			return (n);
+			n--;
+		}
+
+
+	
+	}
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			return (n);
+			n++;
+		}
+	}
+}
