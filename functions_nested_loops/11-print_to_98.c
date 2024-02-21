f640498e07c812dc17cf50cf121b9b44adfdd09b
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-21 15:12:22 +0000

    function that print numbers from n to 98 using printf from stdlib

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -13,7 +13,14 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
+			if (n > 98)
+			{
 			printf ("%d, ", n);
+			}
+			else
+			{
+				printf ("%d", n);
+			}
 			n--;
 		}
 
@@ -24,7 +31,14 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
+			if (n < 98)
+			{
 			printf ("%d, ", n);
+			}
+			else
+			{
+				printf ("%d", n);
+			}
 			n++;
 		}
 	}
