92b86aa05917f49513b58387809f500df3773e29
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-21 09:25:36 +0000

    push a function that will computes value of an integer

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,9 +2,7 @@
 
 /**
  * _abs - check for absolute value
- *
  * Description: 'check if c is an alphabetical character'
- *
  * @r : number to check
  * always return 0
  */
@@ -14,11 +12,11 @@ int _abs(int r)
 	if (r < 0)
 	{
 		r = -r;
-		return(r);
+		return (r);
 	}
 	else
 	{
-		return(r);
+		return (r);
 	}
 	return (0);
 
