76cd6c02402cbf64bfe37efdb441cad28f7b7fd1
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-21 12:21:47 +0000

    function that prints the 9 times table, starting with 0.

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - check for absolute value
+ * Descriptio: 'check if c is an alphabetical character'
+ * @r: number to check
+ * Return: 0 if code ok
+ */
+
+void times_table(void)
+{
+int n;
+int r;
+	for (n = 0; n <= 10; n++)	
+	{
+		r = n * 9;
+		return (r);
+}
