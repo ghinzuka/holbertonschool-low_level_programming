72c6b85807d764088bc0b3eb77037f53b567d1ee
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-23 08:47:08 +0000

    write function

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main (void)
+{
+
+write (int fd, void "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", size_t 59);
+
+return(1);
+
+}
