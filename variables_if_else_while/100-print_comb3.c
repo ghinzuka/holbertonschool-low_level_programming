8aa678fbe509790858aeeed3cbb63141bc0c2ca4
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-23 09:06:50 +0000

    print combinaison of two digits using putchar and for loop

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+
+/**
+ * main - main functon
+ *
+ * Description: 'prints all possible different combinations of two digits.'
+ *
+ * Return: Always return  0
+ */
+
+int main(void)
+
+{
+	int n;
+	int m;
+
+	for (n = 0, n <= 9, n++)
+	{
+		for (m = 0, m <=9, m++)
+		{
+			if ( n != m)
+			{
+				putchar('0' + n);
+				putchar('0' + m);
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+
+}
