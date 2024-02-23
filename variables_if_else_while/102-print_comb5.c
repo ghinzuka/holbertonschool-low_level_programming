5124c60ffd91d267faee51c2910bdba855b0f6b4
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-23 10:54:24 +0000

    Write a program that prints all possible combinations of two two-digit numbers.

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * main - show every minute from 00:00 to 23:59
+ * Return: 0 if code ok
+ */
+
+int main(void)
+{
+	int first, second;
+
+	for (first = 0; first <= 99; first++)
+	{
+		for (second = (first + 1); second <= 99; second++)
+		{
+		putchar((first / 10) + '0');
+		putchar((first % 10) + '0');
+		putchar(' ');
+		putchar((second / 10) + '0');
+		putchar((second % 10) + '0');
+			if (first < 98) 
+			{
+			putchar(',');
+			putchar(' ');
+			}
+		}
+
+	}
+putchar('\n');
+return (0);
+}
