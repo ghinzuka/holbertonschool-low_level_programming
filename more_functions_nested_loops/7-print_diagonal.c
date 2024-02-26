8c69f6c5ca84b5cf930df420699f7452b9ee5e0d
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-26 10:52:06 +0000

    wirte a diagonale

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,21 @@
 void print_diagonal(int n)
 {
 	int i;
-
+	int j;
 if (n > 0)
 {
+	for (j = 1; j <= n; j++)
+	{
 		for (i = 1; i <= n; i++)
 		{
 		_putchar('\\');
 		_putchar('\n');
 		}
+		if (j >= 2)
+		{
+		_putchar(' ');
+		}
+	}
 }
 else
 {
