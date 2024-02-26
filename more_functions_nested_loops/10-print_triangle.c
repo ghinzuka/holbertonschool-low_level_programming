e5f29f807116d9221183ba2c5ed760562d133a57
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-26 13:01:18 +0000

    prints a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_triangle - function that print a triangle
+ *
+ * @size: size of the triangle
+ *
+ * Return: 0 for success
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int b;
+
+	if (size >= 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (b = 1 b <= size; b++)
+			{
+				_putchar("#");
+			}
+			_putchar("\n");
+		}
+	}
+	else 
+	{
+	_putchar("\n");
+	}
+}
