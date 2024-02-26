8ce0204cacb78ed966876453fd2522ee0460a47a
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-26 09:50:44 +0000

    print number from 0 to 9 except 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - check the code
+ *
+ *
+ */
+void print_most_numbers(void)
+{
+int i = 0;
+for (i = 0; i <= 9; i++)
+	{
+		if (i != 2 && != 4)
+		{
+	_putchar('0' + i);
+		}
+	}
+_putchar('\n');
+}
