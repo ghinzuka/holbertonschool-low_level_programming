a4d50c7dc5a4636b790e7d6cd7fc6076a622f3b9
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-26 10:41:27 +0000

    wirte a diagonale

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonale of \  n times
+ * @n: number of lines
+ */
+
+void print_diagonal(int n)
+{
+	int j;
+	int i;
+
+if (n > 0)
+{
+	for (j = 1; j <= n; j++)
+	{
+		for (i = 1; i <= n; i++)
+		{
+		_putchar(134);
+		}
+	}
+	_putchar('\n');
+}
+else
+{
+	_putchar('\n');
+}
+}
