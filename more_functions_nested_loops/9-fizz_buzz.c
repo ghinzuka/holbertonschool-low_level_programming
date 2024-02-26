222759299656ead5224c7ba9f44711a0912ab6cc
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-26 12:23:11 +0000

    fizzBuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main (void)
 	int i;
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3) == 0)
 		{
 			printf("Fizz ");
 		}
@@ -20,7 +20,7 @@ int main (void)
 		{
 			printf("FizzBuzz ");
 		}
-		else if (i % 5 == 0)
+		else if ((i % 5) == 0)
 		{
 			printf("Buzz ");
 		}
