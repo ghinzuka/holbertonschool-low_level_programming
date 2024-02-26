268aae0ba4209ebd0ce15b82a750e4f71c1c64f2
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-26 09:05:25 +0000

    check if c is in uppercase using a && operator

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+ * _islower - check for uppercase
+ *
+ * Description: 'check for uppercase'
+ *  @c: character to check
+ *
+ * Return: 1 if uppercase
+ * Return 0 otherwise
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	return (0);
+
+}
