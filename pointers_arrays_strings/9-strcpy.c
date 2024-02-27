75ee5a01a20ec87ec5ffa703dca838d01f2b05da
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-27 21:59:39 +0000

    using a while loop, two increment for the source and destination until the 0

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -8,13 +8,18 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	while (*src != '\0')
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+
+	while (*src != (i + 1))
 	{
 		*dest = *src;
 		src++;
 		dest++;
 	}
-
-	*dest = '\0';
 	return (dest);
 }
