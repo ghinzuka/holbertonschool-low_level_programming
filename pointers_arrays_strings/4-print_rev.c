68067e9241af0d3f0592aadb6dcbc57ab5d9933d
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-27 11:15:50 +0000

    fucntion that print in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - print a strin in reverse
+ *
+ *@s: string to check
+ *
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int lenght = 0;
+
+	while (*s++)
+	{
+		lenght++;
+	}
+	i = lenght + 1;
+
+		while (i >= 0)
+		{
+			_putchar(*s);
+			i--;
+			s--;
+		}
+	_putchar('\n');
+
+}
