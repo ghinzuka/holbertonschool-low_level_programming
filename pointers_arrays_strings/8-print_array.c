a0b78b919e878318ab8e06f3fd2a591167546adb
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-27 21:15:22 +0000

    print the value of an array of N elements

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_array - print n element of an array
+ *
+ * @a: value of variable
+ * @n: number of elements of the array
+ */
+void print_array(int *a, int n)
+{
+int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	putchar('\n');
+}
