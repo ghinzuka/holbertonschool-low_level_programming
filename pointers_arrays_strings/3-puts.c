4465bf9bc62841327f2fad33e4fc3753f0cdd888
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-27 10:26:12 +0000

    lol

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string folowed by new line to stdout
+ *
+ * @str: string to prints
+ *
+ */
+
+void _puts(char *str)
+{
+	while (*str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
