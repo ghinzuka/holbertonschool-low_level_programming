4d2647e0bfcc055fdf3b3d5d2107700189941d48
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-27 08:50:01 +0000

    use a pointer to update the value of a variable

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,14 +3,11 @@
 /**
  * reset_to_98 - function point to a int and update the value to 98
  *
- *@*n: pointer
+ *@n: pointer
  *
- * @n: variable int
  */
 
 void reset_to_98(int *n)
 {
-	int n = 0;
-	*n = &n;
 	*n = 98;
 }
