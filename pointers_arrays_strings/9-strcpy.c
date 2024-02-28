ffd873e4039786188c5b0cd2bb202e70660b4383
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-28 09:36:54 +0000

    resolving the null problem

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
+	while (*src != '\0')
+	{	
+		*dest = *src;
+		dest++;
+		src++;
 	}
+	*dest = '\0';
 	
 	return (dest);
 }
