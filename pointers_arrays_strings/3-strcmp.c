4b9b7d1c4a5cdf3a4cddf310c890ba55a51e3dd4
ghinzuka <baptiste.pouquerou@gmail.com>
2024-02-29 05:26:36 +0000

    fixing number of the return value

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -13,18 +13,14 @@ int _strcmp(char *s1, char *s2)
 
 	for (i = 0, j = 0; s1[i] && s2[j]; i++, j++)
 	{
-	;
-	}
-
-	if (i < j)
-	{
-		return (-1);
-	}
-	else if (i == j)
+	if (s1[i] == s2[j])
 	{
 		return (0);
 	}
-	else
-		return (1);
-
+	else if (s1[i] != s2[j])
+	{
+		return (s1[i] - s2[j]);
+	}
+	}
+return(0);
 }
