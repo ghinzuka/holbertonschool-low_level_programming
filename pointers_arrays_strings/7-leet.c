6a8106d55a2b37cbe589dba9df718abce7e43bc4
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-01 13:34:00 +0000

    using two loops and a if with two char array

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -6,20 +6,20 @@
  */
 char *leet(char *a)
 {
-	char car [] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L', '\0'};
-	int number [] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1', '\0'};
-	int i;
-	int b;
+char car [] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L', '\0'};
+int number [] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1', '\0'};
+int i;
+int b;
 
-	for (i = 0; a[i] != '\0'; i++)
+for (i = 0; a[i] != '\0'; i++)
+{
+	for (b = 0; b < 11; b++)
 	{
-			for (b = 0; b < 11; b++)
-			{ 
-				if (a[i] == car[b])
-				{
-					a[i] = number[b];
-				}
-			}
+		if (a[i] == car[b])
+		{
+			a[i] = number[b];
+		}
 	}
-	return (a);
+}
+return (a);
 }
