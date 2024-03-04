d3a4ecc6a4fa4f44c6367814387f9a25ae5bb7ef
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-04 10:55:13 +0000

    fires try using two loop while

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strspn - function that gets the lenght of a prefix substring
+ * @s: initiale segment
+ * @accept: byte to gets
+ * Return: the number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int len = 0;
+
+	while (*s++)
+	{
+		while (*accept++)
+		{
+			if (*accept == *s)
+			{
+				len = len + 1;
+			}
+		}
+	}
+	return (len);
+}
