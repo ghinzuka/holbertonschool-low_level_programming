54a12c4ebd9bd24d8588a6b0c7ec4e843c824f23
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-05 13:59:28 +0000

    using the recursive method to show the last leter first and then going up to the start

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -2,6 +2,7 @@
 /**
  * print_rec_recusrion - function that prints a string in reverse
  * @s: string to reverse
+ * Return: void
  */
 void _print_rev_recursion(char *s)
 {
@@ -10,5 +11,4 @@ void _print_rev_recursion(char *s)
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
-	_putchar('\n');
 }
