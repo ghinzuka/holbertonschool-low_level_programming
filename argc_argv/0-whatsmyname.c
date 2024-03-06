9128a1195264fe419be45244b298a53d07b807df
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-06 14:53:36 +0000

    using a printf command to show the first case of the array argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 /**
  * main - print the function name
- * @argc: size of argv 
+ * @argc: size of argv
  * @argv: array of string
  * Return: 0 if ok
  */
 int main(int argc, char *argv[])
-{	
+{
 	printf("the program name is %s\n", argv[0]);
 	return (argc);
 }
