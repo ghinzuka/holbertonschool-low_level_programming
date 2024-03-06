ced64729f18a01104d36e6966cb256fbdacc15cb
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-06 08:33:17 +0000

    correction initialing int res; instead of int res =0;

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -7,16 +7,19 @@
  */
 int _pow_recursion(int x, int y)
 {
-	int res = 0;
+	int res;
 
 	if ( y < 0)
 	{
 		return (-1);
 	}
-	if (res == 0)
+	if (y == 0)
 	{
 		return (1);
 	}
+	if ( y > 0)
+	{
 	res = (x * _pow_recursion(x, y - 1));
+	}
 return (res);
 }
