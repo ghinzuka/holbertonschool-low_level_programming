c7c156f5c53234251ca7ac29e22e841c109bbc78
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-06 08:23:53 +0000

    using a fractionnal to multiply x by teh fractionnal of y

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _pow_recursion - function that returns the value of x to y
+ *@x: first int
+ *@y: second int
+ * Return: the result
+ */
+int _pow_recursion(int x, int y)
+{
+	int res = 0;
+
+	if ( y < 0)
+	{
+		return (-1);
+	}
+	if (res == 0)
+	{
+		return (1);
+	}
+	res = (x * _pow_recursion(x, y - 1));
+return (res);
+}
