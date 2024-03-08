c625e4534acec62210333ab03a084ccae0c621d3
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-08 13:16:36 +0000

    using one loop to add the variable i being argv[i] and another loop to control character in argv[i][j]

---
+++ argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * main - add two positives number
+ * @argc: number of argument
+ * @argv: array of argc
+ * Result: 0 if ok 1 if not
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int result = 0;
+
+	if (argc < 1)
+	{
+		printf("%d\n", 0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+		{
+			if(!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+                                return (1);
+			}
+		}
+		result += atoi(argv[i]);
+	}
+printf("%d\n", result);
+return (0);
+}
