21a8a509d84c6bfadbcfc95225eb8a85ace3ae74
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-11 09:50:46 +0000

    firs push

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * create_array - creates an array of chars initializes it with a char
+ * @size: first variable
+ * @c: char
+ * Return: 0 if ok or NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *b;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	b = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+	{
+		b[i] = c;
+	}
+
+	if (b == NULL)
+	{
+		return (NULL);
+	}
+
+free(b);
+return (b);
+}
