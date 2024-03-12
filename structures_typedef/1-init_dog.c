82bcbcc4e8f99074ae6a03471ccb0b8e4fb4135d
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-12 16:09:46 +0000

    initiaiazing using -> method istead of (*d).name and correcting the if the D!= NULL

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,15 +1,17 @@
 #include "dog.h"
 /**
  * init_dog - initialize dog
- * type dog
- * @d: dog
+ * @d: pointer to the dog in the header
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+if (d != NULL)
+{
 d->name = name;
 d->age = age;
 d->owner = owner;
+}
 }
