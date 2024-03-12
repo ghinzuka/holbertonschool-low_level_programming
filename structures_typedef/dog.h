766df03a90090a1de4cd54a7c5b87b4f3ab5e407
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-12 15:07:15 +0000

    push first time cause does not compile

---
+++ structures_typedef/dog.h
@@ -0,0 +1,25 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dog - structure with 3 information in it
+ * @name: name
+ * @age: age of the dog
+ * @owner: dog's owner
+ */
+typedef struct dog
+{
+*char name;
+float age;
+*char owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
