5e2c5b9b8083e72a231981610a31d741dee95da3
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-13 08:34:33 +0000

    using typedef if the header

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,12 +10,17 @@
  * @age: age of the dog
  * @owner: dog's owner
  */
+
+typedef struct dog dog_t;
+
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
