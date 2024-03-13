cd491f546655b91915b503cd0aff2af8db8f46b2
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-13 13:35:12 +0000

    using malloc to alocate dynamic space to use the structure in header, and a strlen program to count the size of name and owner, copying the the new name if the old name using a for loop

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,22 +4,38 @@
  * @name: new dog's name
  * @age: new dog age
  * @owner: new dog's owner
+ * Return: the newdog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *firstdog;
 unsigned int len1, len2, i = 0;
 
-if(
+if (name == NULL || age < 0 || owner == NULL)
+	return (NULL);
 
 for (i = 0; name[i]; i++)
 	len1++;
 for (i = 0; owner[i]; i++)
 	len2++;
+
 firstdog = malloc(sizeof(dog_t));
 if (firstdog == NULL)
-{
 	return (NULL);
-}
 
+firstdog->age = age;
+
+firstdog->name = malloc(len1 * sizeof(firstdog->name));
+if (firstdog->name == NULL)
+return (NULL);
+for (i = 0; i < len1; i++)
+firstdog->name[i] = name[i];
+
+firstdog->owner = malloc(len2 * sizeof(firstdog->owner));
+if (firstdog->owner == NULL)
+return (NULL);
+for (i = 0; i < len2; i++)
+firstdog->owner[i] = owner[i];
+
+return (firstdog);
 }
