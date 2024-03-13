7d6dcb6001e5b864e47023b9c289de421c00fa43
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-13 07:57:58 +0000

    correction of new line

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("Name: %s", d->name);
+		printf("Name: %s\n", d->name);
 	}
 	if (d->age <= 0)
 	{
@@ -23,7 +23,7 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("Age: %f", d->age);
+		printf("Age: %f\n", d->age);
 	}
 	if (d->owner == NULL)
 	{
@@ -31,6 +31,6 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		printf("Owner: %s", d->owner);
+		printf("Owner: %s\n", d->owner);
 	}
 }
