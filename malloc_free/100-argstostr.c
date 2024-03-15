784ab33894013f183ac0308eb337b552d8b04193
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-15 08:38:10 +0000

    using two loops to count number of char - using malloc to create the momory in heap and  two loops again to give the malloc pointers the results

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * argstostr - function that concatenates all arguments of a program
+ * @ac: arguments
+ * @av: numbers of char to memory
+ * Return: NULL if fail or the pointer
+ */
+char *argstostr(int ac, char **av)
+{
+int i, j, len = 0;
+char *c;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			len++;
+		}
+	}
+
+	c = malloc(sizeof(char) * len);
+	if (c == NULL)
+	{
+		return (NULL);
+	}
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			c[len] = av[i][j];
+			len++;
+		}
+			c[len++] = '\n';
+	}
+
+	return (c);
+
+}
