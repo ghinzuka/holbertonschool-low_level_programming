4d323141ac23c0404dc6e9fda88627f1609f73d4
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-18 10:22:36 +0000

    using a while loop and decreminting for size of array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - executes a function given as a parameter on each element of an array
+ * @array: array
+ * @size: size of array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+	size_t i = size;
+
+	while (size > 0)
+	{
+		action(array[i - size]);
+		size--;
+	}
+}
