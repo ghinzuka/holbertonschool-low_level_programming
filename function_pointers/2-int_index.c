1bcb922b78b74928da26cae1c661eba23742ec10
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-18 13:21:54 +0000

    using a loop

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that search for an integer
+ * @array: array to look in
+ * @size: size of array
+ * @cmp: function that compare
+ * Return: i
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+
+if (array)
+{
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+		{
+			return (i);
+		}
+	}
+}
+return (-1);
+}
