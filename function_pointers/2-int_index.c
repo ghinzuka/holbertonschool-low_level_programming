150f52033fef87ecd8ff8fad5f6276aae6dbded7
ghinzuka <baptiste.pouquerou@gmail.com>
2024-03-18 13:26:35 +0000

    using a loop

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -10,12 +10,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 
+if (size <= 0)
+{
+	return (-1);
+}
+
 if (array)
 {
-	if (size <= 0)
-	{
-		return (-1);
-	}
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
