ff373ee3ae224987df458af9c438bfb9e636e73b
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-02 14:19:03 +0200

    loop to all the node until NULL and free the main list while the next one is safe in temp

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_list - frees a list_t list.
+ * @head: first node
+ */
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+		list_t *temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
