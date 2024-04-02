6c51823c306fe9167a92dd1101aa6dd472132b91
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-02 10:29:09 +0200

    try a method inverting the if statement

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,16 +11,16 @@ size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (h->str != NULL)
+		if (h->str == NULL)
 		{
-			printf("[%d] %s\n", h->len, h->str);
+            printf("[0] (nil)\n");
 		}
 
 		else
 		{
-			printf("[0] (nil)\n");
+            printf("[%d] %s\n", h->len, h->str);
 		}
 		n++;
 		h = h->next;
