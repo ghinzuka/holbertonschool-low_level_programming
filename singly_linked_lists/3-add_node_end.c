fc058ef2ef52cb8ed83b802ea215a2fab7c1a3e0
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-02 12:09:36 +0200

    fixing betty for the header

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+#include <string.h>
+/**
+ * add_node_end - adds a new node at the end of list
+ * @head: pointer the head of the list
+ * @str: string to copy
+ * Return: NULL if function fail or the adress of new element
+*/
+list_t *add_node_end(list_t **head, const char *str)
+{
+	int len = 0;
+	
+	list_t *new = malloc(sizeof (list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		return (NULL);
+	}
+	while (str[len])
+	{
+		len++;
+	}
+	new->len = len;
+	new->next = NULL;
+
+
+
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -5,22 +5,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
  * Description: singly linked list node structure
  */
 
-
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
@@ -30,4 +27,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _putchar(char c);
 
-#endif 
+#endif
