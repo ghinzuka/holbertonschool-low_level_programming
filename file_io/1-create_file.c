ee4481ab2738003e27bf4027f79454b0785f4007
ghinzuka <baptiste.pouquerou@gmail.com>
2024-04-05 10:57:53 +0200

    using open system call to open with flags : creat if not presen, rdwr to read and write, trunc if no empty, and give autorisation to own with two flags

---
+++ file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file.
+ * @text_content: NULL terminated string to write to the file
+ * @filename: file to create
+ * Return: 1 on success, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int len, fo, fw;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	while (len[text_content])
+	{
+		len++;
+	}
+
+	fo = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fo == -1)
+	{
+		return (-1);
+	}
+	fw = write(fo, text_content, len);
+	if (fw == -1)
+	{
+		return (-1);
+	}
+
+	close(fo);
+	return (1);
+}
